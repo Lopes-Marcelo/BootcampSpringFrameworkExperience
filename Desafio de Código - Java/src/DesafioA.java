c368695779c2e35d2d000fe7b7db6053d15ff739
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-07-19 00:11:27 -0400

    Desafio A - Completo

--- Desafio de Código - Java/src/DesafioA.java
+++ Desafio de Código - Java/src/DesafioA.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 
 public class DesafioA {
     public static void main(String[] args) {
+
         Scanner teclado = new Scanner(System.in);
-// TODO: complete os espaços em branco com sua solução para o problema
-//    	String[] nomes = new String[     ];
-		
-		for(int i=0 ; i<10 ; i++){
-//            nomes[    ] = sc.nextLine();
+
+        String[] nomes = new String[10];
+
+        for(int i=0 ; i <10 ; i++){
+
+            nomes[i] = teclado.nextLine();
+
         }
+        System.out.println(nomes[2]);
+        System.out.println(nomes[6]);
+        System.out.println(nomes[8]);
 
         teclado.close();
     }
