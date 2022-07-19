d7ba9585b3f1dbbca13dd42e90ee0551bc91b953
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-07-19 17:26:57 -0400

    Projeto Finalizado

---
+++ Desafio de Projeto - Java Spring/academia-javaspring/src/main/java/com/academia/javaspring/academiajavaspring/AcademiaJavaspringApplication.java
@@ -0,0 +1,13 @@
+package com.academia.javaspring.academiajavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaJavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaJavaspringApplication.class, args);
+	}
+
+}
---
+++ Desafio de Projeto - Java Spring/academia-javaspring/src/test/java/com/academia/javaspring/academiajavaspring/AcademiaJavaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.javaspring.academiajavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaJavaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
