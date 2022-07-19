6ca0ee3ca9f4bb9a703ed0871debe2e7af01e13c
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-07-18 22:53:14 -0400

    Desafio D - Completo

---
+++ Desafio de Código - Java/src/DesafioB.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class DesafioB {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+       
+        int casos = teclado.nextInt();
+        int cont = 0;
+        while (cont < casos){
+ 
+            double a = teclado.nextDouble();
+            double b = teclado.nextDouble();
+            double c = teclado.nextDouble();
+	
+	   // TODO: complete os espaços em branco com sua solução para o problema
+      
+
+
+              
+            cont++;
+        }
+        teclado.close();
+    }
+}
+
+/* 
+Desafio:
+Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem peso 5.
+
+Entrada:
+O arquivo de entrada contém um valor inteiro N na primeira linha. Cada N linha a seguir contém um caso de teste com três valores com uma casa decimal cada valor.
+
+Saída:
+Para cada caso de teste, imprima a média ponderada dos 3 valores, conforme exemplo abaixo.
+*/
--- Desafio de Código - Java/src/DesafioD.java
+++ Desafio de Código - Java/src/DesafioD.java
@@ -33,9 +33,12 @@ public class DesafioD{
 
 }
 
-    /*O objetivo deste desafio é resolvê-lo utilizando o seu conhecimento sobre Arrays, uma estrutura de dados que armazena uma coleção de dados em um bloco de memória.
+/*
+Desafio:
+O objetivo deste desafio é resolvê-lo utilizando o seu conhecimento sobre Arrays, uma estrutura de dados que armazena uma coleção de dados em um bloco de memória.
 
-    Você está desenvolvendo um sistema para um local de eventos, neste local, é necessário que a entrada seja permitida apenas para pessoas acima de 18 anos. Faça um programa para ler um número N, a idade de N pessoas. Depois disso, coloque a idade mínima permitida  para se entrar no local, conforme exemplo. 
+Você está desenvolvendo um sistema para um local de eventos, neste local, é necessário que a entrada seja permitida apenas para pessoas acima de 18 anos. Faça um programa para ler um número N, a idade de N pessoas. Depois disso, coloque a idade mínima permitida  para se entrar no local, conforme exemplo. 
     
-    Entrada: 4 18 22 15 50  
-    Saída: 4 15*/
+Entrada: 4 18 22 15 50  
+Saída: 4 15
+*/
